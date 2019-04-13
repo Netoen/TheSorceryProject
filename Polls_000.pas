d36fa1d88dc7312670cdf8d9c98a210eab9f1864
necrontoend@gmail.com <Netoen>
2019-04-13 13:06:39 +0700

    First git commit

---
+++ Polls_000.pas
@@ -0,0 +1,41 @@
+unit Polls;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Samples.Spin;
+
+type
+  TForm4 = class(TForm)
+    SpinEdit1: TSpinEdit;
+    SpinEdit2: TSpinEdit;
+    SpinEdit3: TSpinEdit;
+    SpinEdit4: TSpinEdit;
+    SpinEdit5: TSpinEdit;
+    SpinEdit6: TSpinEdit;
+    SpinEdit7: TSpinEdit;
+    SpinEdit8: TSpinEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.Button1Click(Sender: TObject);
+var i: byte;
+begin
+  for i := 1 to 8 do
+   (Form4.FindComponent('SpinEdit' + IntToStr(i)) as TSpinEdit).Value := 0;
+end;
+
+end.
